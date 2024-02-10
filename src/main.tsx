00f466a9c61bcaf68fc032dd46ab23afad0bc641
steroyle <steroyle@gmail.com>
2024-02-10 14:19:59 +0000

    Added mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import './index.css';
+import '@mantine/core/styles.css';
+import {MantineProvider} from '@mantine/core';
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
